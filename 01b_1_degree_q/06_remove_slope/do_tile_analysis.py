5810b2f7c5206616e841bd7511071a38a35b2c02
HeatherKmtb <heather.kaymtb@gmail.com>
2023-02-06 16:30:24 +0000

    adding 01b/06

---
+++ 01b_1_degree_q/06_remove_slope/do_tile_analysis.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 11:13:39 2022
+
+@author: heatherkay
+"""
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os.path
+import geopandas
+import numpy as np
+from rsgislib import vectorutils
+
+logger = logging.getLogger(__name__)
+
+class ProcessJob(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='perform_processing.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        file = self.params["gedi_file"]
+        out_file = self.params["out_file"]
+                       
+        beams = vectorutils.get_vec_lyrs_lst(file)
+
+        for beam in beams:
+            df = geopandas.read_file(file, layer=beam)
+            new = df[df['slope']<10]
+            if new.empty:
+                continue
+            new.to_file(out_file, layer = beam, driver='GPKG')
+
+
+    def required_fields(self, **kwargs):
+        return ["gedi_file","out_file"]
+
+    def outputs_present(self, **kwargs):
+        return True, dict()
+
+    def remove_outputs(self, **kwargs):
+        print("No outputs to remove")
+
+if __name__ == "__main__":
+    ProcessJob().std_run()
