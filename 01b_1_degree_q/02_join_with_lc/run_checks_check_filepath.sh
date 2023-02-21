12a491b2c4a6ff65ea6aa59493a54ab020ae4508
Heather <heather.kaymtb@gmail.com>
2023-02-21 09:07:30 +0000

    adding 02/01 scripts

--- 01b_1_degree_q/02_join_with_lc/run_checks_check_filepath.sh
+++ 01b_1_degree_q/02_join_with_lc/run_checks_check_filepath.sh
@@ -1,2 +1,2 @@
-singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif rsgischkgdalfile.py -i "/scratch/a.hek4/data/1_deg_q/2.join_lc/GEDI02_B_2019_Q2/*.gpkg" --vec --rmerr --printerr --epsg 4326 --chkproj
+singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif rsgischkgdalfile.py -i "/scratch/a.hek4/data/1_deg_q/3.remove_lc_cats/GEDI02_B_2021_Q3/*.gpkg" --vec --rmerr --printerr --epsg 4326 --chkproj
 
--- 02_final/01_grid_ony/gen_jobs.py
+++ 02_final/01_grid_ony/gen_jobs.py
@@ -1,51 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Mon Dec  5 11:10:09 2022
+
+@author: heatherkay
+"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Nov 30 13:52:32 2022
 
 @author: heatherkay
 """
 from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import os.path
 import logging
 import glob
-import rsgislib
-import rsgislib.vectorattrs
-import os
 
 logger = logging.getLogger(__name__)
 
 class GenCmds(PBPTGenQProcessToolCmds):
 
     def gen_command_info(self, **kwargs):
-        if not os.path.exists(kwargs['out_dir']):
-            os.mkdir(kwargs['out_dir'])
+        if not os.path.exists(kwargs['out_fig_dir']):
+            os.mkdir(kwargs['out_fig_dir'])
+            
+        if not os.path.exists(kwargs['out_csv_dir']):
+            os.mkdir(kwargs['out_csv_dir'])
 
         gedi_files = glob.glob(kwargs['gedi_tiles'])
+        
 
         for gedi_file in gedi_files:
             basename = self.get_file_basename(gedi_file)
-            out_file = os.path.join(kwargs['out_dir'], f'{basename}.gpkg')
+            out_csv_file = os.path.join(kwargs['out_csv_dir'], f'{basename}.csv')
 
-            if (not os.path.exists(out_file)):
+            if (not os.path.exists(out_csv_file)):
                 c_dict = dict()
                 c_dict['gedi_file'] = gedi_file
-                c_dict['out_file'] = out_file
-                c_dict['raster'] = '/scratch/a.hek4/layers/PROBAV_LC100_global_v3.0.1_2019-nrt_Discrete-Classification-map_EPSG-4326.tif'
+                c_dict['out_fig_dir'] = '/scratch/a.hek4/results/1_deg/figs/all/'
+                c_dict['out_csv_file'] = out_csv_file
+                #c_dict['quarter'] = '2020_Q1'
+                #c_dict['results'] = results
                 self.params.append(c_dict)
 
 
     def run_gen_commands(self):
         self.gen_command_info(
-            gedi_tiles='/scratch/a.hek4/data/1_deg_q/1.filter_for_quality/GEDI02_B_2020_Q1/*.gpkg',
-            out_dir='/scratch/a.hek4/data/1_deg_q/2.join_lc/GEDI02_B_2020_Q1')
-
-
+            gedi_tiles='/scratch/a.hek4/data/1_deg_q/9-all_joined/*.gpkg',
+            out_fig_dir='/scratch/a.hek4/results/1_deg/figs/all/',
+            out_csv_dir='/scratch/a.hek4/results/1_deg/csvs/all/')
+            #ALSO CHANGE QUARTER AND OUT_FIG_DIR IN C_DICT ABOVE
+        
         self.pop_params_db()
-        self.create_slurm_sub_sh("join_lc", 8224, '/scratch/a.hek4/logs', run_script="run_exe_analysis.sh",
+
+        self.create_slurm_sub_sh("join_lc", 8224, '/scratch/a.hek4/logs', run_script="exe_analysis.sh",
                                   db_info_file=None, account_name='scw1403', n_cores_per_job=5, n_jobs=5, job_time_limit='2-23:59',
                                   module_load='module load parallel singularity\n')
-        #self.create_shell_exe(run_script="run_exe_analysis.sh", cmds_sh_file="cmds_lst.sh", n_cores=25, db_info_file="pbpt_db_info_lcl_file.txt")
-
+        
 if __name__ == "__main__":
     py_script = os.path.abspath("do_tile_analysis.py")
     script_cmd = "singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif python {}".format(py_script)
@@ -55,5 +67,5 @@ if __name__ == "__main__":
 
     create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.hek4/pbpt_db_info.txt",
                            lock_file_path="/scratch/a.hek4/tmp/gedi_lock_file.txt",
-                           process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+                           process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)   
     create_tools.parse_cmds()
