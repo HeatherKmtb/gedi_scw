c33a2f15d9a2dfd1158483e9f7bef14e75030c85
Heather <heather.kaymtb@gmail.com>
2023-06-08 12:52:44 +0100

    adjusting 02/02c for over 10m

--- 02_final/02c_stack_csvs_wwf/stack_csvs.py
+++ 02_final/02c_stack_csvs_wwf/stack_csvs.py
@@ -1,8 +1,8 @@
 import glob
 import pandas as pd
 
-csvs = glob.glob('/scratch/a.hek4/results/1_deg/csvs/wwf_grid/*.csv')
-out_file = '/scratch/a.hek4/results/1_deg/csvs/wwf_grid.csv'
+csvs = glob.glob('/scratch/a.hek4/results/1_deg/csvs/wwf_grid/10m/*.csv')
+out_file = '/scratch/a.hek4/results/1_deg/csvs/wwf_grid_10m.csv'
 
 
 dfList = [pd.read_csv(c) for c in csvs]
