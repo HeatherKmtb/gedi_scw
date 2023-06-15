9e52b178f1c2ef974253a9b9969005476a9b8aec
Heather <heather.kaymtb@gmail.com>
2023-06-15 11:14:12 +0100

    changing roi file in 00d/00/01

--- 00d_1km_grid/00_create_1km_grid/01_get_bboxes/process_script.py
+++ 00d_1km_grid/00_create_1km_grid/01_get_bboxes/process_script.py
@@ -4,8 +4,8 @@ import rsgislib.vectorattrs
 
 glb_tiles_vec_file = "/scratch/a.hek4/data/1km/5deg_grid.gpkg"
 glb_tiles_vec_lyr = "grid"
-glb_land_vec_file = "/scratch/a.hek4/gadm36_levels.gpkg"
-glb_land_vec_lyr = "level0"
+glb_land_vec_file = "/scratch/a.hek4/gedi_scw/glb_land_roi_deg_tiles_named.geojson"
+glb_land_vec_lyr = "glb_land_roi_deg_tiles_named"
 
 #glb_land_tiles_vec_file = "glb_land_deg_tiles.geojson"
 #glb_land_tiles_vec_lyr = "glb_land_deg_tiles"
