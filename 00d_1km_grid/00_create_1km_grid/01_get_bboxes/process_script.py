4ceeaa12e33515af68292c76470eb4d3595148a3
Heather <heather.kaymtb@gmail.com>
2023-06-15 10:55:09 +0100

    adding 00d/00/01

---
+++ 00d_1km_grid/00_create_1km_grid/01_get_bboxes/process_script.py
@@ -0,0 +1,25 @@
+
+import rsgislib.vectorutils
+import rsgislib.vectorattrs
+
+glb_tiles_vec_file = "/scratch/a.hek4/data/1km/5deg_grid.gpkg"
+glb_tiles_vec_lyr = "grid"
+glb_land_vec_file = "/scratch/a.hek4/gadm36_levels.gpkg"
+glb_land_vec_lyr = "level0"
+
+#glb_land_tiles_vec_file = "glb_land_deg_tiles.geojson"
+#glb_land_tiles_vec_lyr = "glb_land_deg_tiles"
+
+roi_file = '/scratch/a.hek4/data/1km/5deg_grid_land.geojson'
+roi_lyr='grid'
+
+print('files read')
+
+rsgislib.vectorutils.spatial_select(glb_tiles_vec_file, glb_tiles_vec_lyr, glb_land_vec_file, glb_land_vec_lyr,
+                                    roi_file, roi_lyr, out_format='GeoJSON')
+
+print('done spatial select')
+
+rsgislib.vectorattrs.pop_bbox_cols(roi_file, roi_lyr, x_min_col='xmin',
+                                   x_max_col='xmax', y_min_col='ymin', y_max_col='ymax')
+
--- 00d_1km_grid/00_interactive_node_scw.sh
+++
@@ -1,3 +0,0 @@
-srun --ntasks=1 --time=48:00:00 --mem-per-cpu=16GB --pty bash
-
-
