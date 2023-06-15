caf2804d959d6cc80189ae36dd4dc354f1b04790
Heather <heather.kaymtb@gmail.com>
2023-06-15 07:37:15 +0100

    adding things to debug 00d/01

--- 00d_1km_grid/01_define_1km_tiles/identify_1km_tiles.py
+++ 00d_1km_grid/01_define_1km_tiles/identify_1km_tiles.py
@@ -13,12 +13,18 @@ glb_land_tiles_vec_lyr = "glb_land_deg_tiles"
 roi_file = '/scratch/a.hek4/data/1km/grid_land.geojson'
 roi_lyr='grid'
 
+print('files read')
+
 rsgislib.vectorutils.spatial_select(glb_tiles_vec_file, glb_tiles_vec_lyr, glb_land_vec_file, glb_land_vec_lyr,
                                     roi_file, roi_lyr, out_format='GeoJSON')
 
+print('done spatial select')
 
 rsgislib.vectorattrs.pop_bbox_cols(roi_file, roi_lyr, x_min_col='xmin',
                                    x_max_col='xmax', y_min_col='ymin', y_max_col='ymax')
+
+print('done bbox cols')
+
 
 out_vec_file = "glb_land_roi_deg_tiles_named_1km.geojson"
 out_vec_lyr = "grid"
