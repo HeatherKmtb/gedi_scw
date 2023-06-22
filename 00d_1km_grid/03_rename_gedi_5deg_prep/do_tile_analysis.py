350734d72f5d63051a88e924e1e1c71442da1151
HeatherKmtb <heather.kaymtb@gmail.com>
2023-06-22 14:54:51 +0100

    preparing 00d/03

---
+++ 00d_1km_grid/03_rename_gedi_5deg_prep/do_tile_analysis.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 08:46:52 2022
+
+@author: heatherkay
+"""
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import geopandas
+import rsgislib.vectorutils
+import rsgislib.vectorattrs
+import geopandas as gpd
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+class DoTileAnalysis(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='do_tile_analysis.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        gedi_file = self.params['gedi_file']
+        out_dir = self.params['out_dir']
+        temp_file = self.params["temp_file"]
+        basename = self.params["basename"]
+                       
+        beams = rsgislib.vectorutils.get_vec_lyrs_lst(gedi_file)
+        
+        for beam in beams:
+            df = gpd.read_file(gedi_file, layer = beam)
+            tiles = df['tile_name_right']
+            tile = tiles[0]
+        
+            df.to_file(out_dir + basename + '_' + tile + '.gpkg', layer = beam)
+            
+        Path(temp_file)    
+
+    def required_fields(self, **kwargs):
+        return ["gedi_file", "out_file", "temp_file", "basename"]
+
+
+    def outputs_present(self, **kwargs):
+        return True, dict()
+
+    def remove_outputs(self, **kwargs):
+        print("No outputs to remove")
+
+if __name__ == "__main__":
+    DoTileAnalysis().std_run()
---
+++ 00d_1km_grid/03_rename_gedi_5deg_prep/gen_jobs.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 13:52:32 2022
+
+@author: heatherkay
+"""
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import glob
+import rsgislib.vectorutils
+import rsgislib.vectorattrs
+import os
+import geopandas as gpd
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['out_dir']):
+            os.mkdir(kwargs['out_dir'])
+            
+        if not os.path.exists(kwargs['temp_dir']):
+            os.mkdir(kwargs['temp_dir'])            
+
+        gedi_files = glob.glob(kwargs['gedi_tiles'])
+        #grid_dir = '/scratch/a.hek4/data/1km/grids/5deg_gridded_named/'
+        
+        #grid = '/scratch/a.hek4/data/1km/glb_land_roi_deg_tiles_named_1km.geojson'
+
+        for gedi_file in gedi_files:
+            basename = self.get_file_basename(gedi_file)
+            temp_file = os.path.join(kwargs['temp_dir'], f'{basename}.gpkg')
+            out_dir = '/scratch/a.hek4/data/1km/2-5deg_named/'
+
+            if (not os.path.exists(temp_file)):
+                c_dict = dict()
+                c_dict['gedi_file'] = gedi_file
+                c_dict['temp_file'] = temp_file
+                c_dict['out_dir'] = out_dir
+                c_dict['basename'] = basename
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(
+            gedi_tiles='/scratch/a.hek4/data/1km/1-5deg_prepped/*.gpkg',
+            temp_dir='scratch/a.hek4/data/1km/temp')
+
+
+        self.pop_params_db()
+        self.create_slurm_sub_sh("join_lc", 8224, '/scratch/a.hek4/logs', run_script="run_exe_analysis.sh",
+                                  db_info_file=None, account_name='scw1403', n_cores_per_job=5, n_jobs=5, job_time_limit='2-23:59',
+                                  module_load='module load parallel singularity\n')
+        #self.create_shell_exe(run_script="run_exe_analysis.sh", cmds_sh_file="cmds_lst.sh", n_cores=25, db_info_file="pbpt_db_info_lcl_file.txt")
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("do_tile_analysis.py")
+    script_cmd = "singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'do_tile_analysis'
+    process_tools_cls = 'DoTileAnalysis'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.hek4/pbpt_db_info.txt",
+                           lock_file_path="/scratch/a.hek4/tmp/gedi_lock_file.txt",
+                           process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
