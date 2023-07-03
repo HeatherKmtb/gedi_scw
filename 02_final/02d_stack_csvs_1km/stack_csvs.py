ea8a6f334c8e62026da823774e646604e3b2feba
HeatherKmtb <heather.kaymtb@gmail.com>
2023-07-03 13:40:55 +0100

    adjusting filepaths in 02/02d

--- 02_final/02d_stack_csvs_1km/stack_csvs.py
+++ 02_final/02d_stack_csvs_1km/stack_csvs.py
@@ -1,8 +1,8 @@
 import glob
 import pandas as pd
 
-csvs = glob.glob('/scratch/a.hek4/results/1_deg/csvs/1km/*.csv')
-out_file = '/scratch/a.hek4/results/1_deg/csvs/1km.csv'
+csvs = glob.glob('/scratch/a.hek4/results/1km/csvs/*.csv')
+out_file = '/scratch/a.hek4/results/1km.csv'
 
 
 dfList = [pd.read_csv(c) for c in csvs]
