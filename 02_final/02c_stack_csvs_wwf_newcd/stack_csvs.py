ad051feeeac0710ed3e47571a108ba7973169b45
HeatherKmtb <heather.kaymtb@gmail.com>
2023-07-04 14:33:14 +0100

    adding 02/02c for new_cd

---
+++ 02_final/02c_stack_csvs_wwf_newcd/stack_csvs.py
@@ -0,0 +1,13 @@
+import glob
+import pandas as pd
+
+csvs = glob.glob('/scratch/a.hek4/results/1_deg/csvs/wwf_grid/new_cd/*.csv')
+out_file = '/scratch/a.hek4/results/1_deg/csvs/wwf_grid_new_cd.csv'
+
+
+dfList = [pd.read_csv(c) for c in csvs]
+    
+df = pd.concat(dfList)  
+df.to_csv(out_file)
+
+
