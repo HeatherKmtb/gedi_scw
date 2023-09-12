4b72d8a60fc0540b7bda1794fed4472ee4ab776e
HeatherKmtb <heather.kaymtb@gmail.com>
2023-09-12 10:48:46 +0100

    adding 01b/08

---
+++ 01b_1_degree_q/08_merge_gpkgs_seasonality/do_tile_analysis.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 11:13:39 2022
+
+@author: heatherkay
+"""
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os.path
+import geopandas as gpd
+import numpy as np
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class ProcessJob(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='perform_processing.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        file = self.params["gedi_file"]
+        out_file = self.params["out_file"]
+
+                       
+        df = gpd.read_file(file)
+        df.to_file(out_file)
+
+
+    def required_fields(self, **kwargs):
+        return ["gedi_file","out_file"]
+
+    def outputs_present(self, **kwargs):
+        return True, dict()
+
+    def remove_outputs(self, **kwargs):
+        print("No outputs to remove")
+
+if __name__ == "__main__":
+    ProcessJob().std_run()
