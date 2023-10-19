4f0549ba92d85b9e9e2995d63d45372a65e8e62d
HeatherKmtb <heather.kaymtb@gmail.com>
2023-10-19 13:00:16 +0100

    adding 02/02g

---
+++ 02_final/02g_stack_csvs_season_cover/stack_csvs.py
@@ -0,0 +1,23 @@
+import glob
+import pandas as pd
+import os
+
+csvs = glob.glob('/scratch/a.hek4/results/1_deg/csvs/cover/*.csv')
+out_file = '/scratch/a.hek4/results/cover.csv'
+
+csv_list = list()
+
+for csv in csvs:
+    try:
+        if os.stat(csv).st_size>0:
+              csv_list.append(csv)
+    except OSError:
+        continue
+
+
+dfList = [pd.read_csv(c) for c in csv_list]
+    
+df = pd.concat(dfList)  
+df.to_csv(out_file)
+
+
